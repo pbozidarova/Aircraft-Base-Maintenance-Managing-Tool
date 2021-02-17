15df68ab7edc92f939bbe8c8247a715d033ffdbe
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-02-17 02:06:35 +0200

    Initial Project Setup

---
+++ src/main/java/managing/tool/AircraftBaseMaintenanceManagingToolApplication.java
@@ -0,0 +1,13 @@
+package managing.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftBaseMaintenanceManagingToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AircraftBaseMaintenanceManagingToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/managing/tool/AircraftBaseMaintenanceManagingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package managing.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftBaseMaintenanceManagingToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
