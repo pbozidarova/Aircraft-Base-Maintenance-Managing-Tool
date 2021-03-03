b1a60c3a6761e76f3e6837c786d49327a8101563
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-03-03 02:41:49 +0200

    Some Materialize front end experiment

--- backend/src/main/java/managing/tool/e_user/web/UserController.java
+++ backend/src/main/java/managing/tool/e_user/web/UserController.java
@@ -2,13 +2,11 @@ package managing.tool.e_user.web;
 
 import managing.tool.e_user.model.dto.UserViewDto;
 import managing.tool.e_user.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost")
 @RestController
 @RequestMapping("/users")
 public class UserController {
