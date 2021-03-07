2c10a9078ad0086c6c94f5bf4987288dddc9423d
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-03-08 01:36:52 +0200

    uploading any changes

--- frontend/abmmt/src/App.js
+++ frontend/abmmt/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './android-chrome-192x192.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        Hello React!
+        Hello ABMM Tool!
 
       </header>
     </div>
